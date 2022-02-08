372a7e4b07ff1d3a73207a9ba0e0144b4e18810f
Miguel Zablah <miguel12979@gmail.com>
2022-02-08 14:50:41 -0600

    [UPDATE] CDN Icon for 0.36.0

--- terragrunt/tools/chocolateyinstall.ps1
+++ terragrunt/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'terragrunt'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$checksum = '37fa57a355e369c01ced4a14fa7f68bd2347659a81635123e1395bd3c44c8d75'
-$url = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.36.1/terragrunt_windows_amd64.exe'
+$checksum = 'a699d2754aaa91f6977724cf4366b71334d49a58f53ac2da175f0e4fcff6e58a'
+$url = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.36.0/terragrunt_windows_amd64.exe'
 $destination = "$toolsPath\terragrunt.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
