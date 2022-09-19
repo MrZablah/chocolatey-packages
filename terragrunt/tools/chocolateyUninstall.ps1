8cd148c41cf0993748ae1de15c3190e391016523
Miguel Zablah <miguel12979@gmail.com>
2022-09-19 13:24:26 -0500

    [FIX] Uninstall Path

--- terragrunt/tools/chocolateyUninstall.ps1
+++ terragrunt/tools/chocolateyUninstall.ps1
@@ -1,5 +1,6 @@
 $packageName = 'terragrunt'
 $fileType = "exe"
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $destination = "$toolsPath\terragrunt.exe"
 
 Uninstall-ChocolateyPackage -PackageName $packageName -FileType $fileType -File $destination
