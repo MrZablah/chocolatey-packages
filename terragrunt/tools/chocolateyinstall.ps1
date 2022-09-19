d7b61050d224c082b5af2c06d355e3c603adb9de
Miguel Zablah <miguel12979@gmail.com>
2022-09-19 10:23:24 -0500

    [UPDATE] Terraform Dependencies

--- terragrunt/tools/chocolateyinstall.ps1
+++ terragrunt/tools/chocolateyinstall.ps1
@@ -6,4 +6,4 @@ $checksum = '81466819216d9954b17c6b411e2de363f84546e755938c61a4b5cf2beae8d639'
 $url = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.38.12/terragrunt_windows_amd64.exe'
 $destination = "$toolsPath\terragrunt.exe"
 
-Install-ChocolateyPackage -PackageName $packageName -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256 -UnzipLocation $destination
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
